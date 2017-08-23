3597b8844a5a4f5e625bba034f23565d3f77f25a
Corniël Joosse <cornieljoosse@gmail.com>
2017-08-23 17:18:01 +0200

    First commit with empty MainActivity and basic SettingsActivity

---
+++ app/src/main/java/pixento/nl/broadcasttomqtt/SettingsFragment.java
@@ -0,0 +1,21 @@
+package pixento.nl.broadcasttomqtt;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+
+    }
+}
