6c880728a187feb32b87f133c09566e632d13094
Corniël Joosse <cornieljoosse@gmail.com>
2017-08-25 14:45:10 +0200

    Added basic MqttConnection class

--- app/src/main/java/pixento/nl/broadcasttomqtt/MainActivity.java
+++ app/src/main/java/pixento/nl/broadcasttomqtt/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MqttConnection connection;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +24,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                connection.publish("Hello World!!");
+                Snackbar.make(view, "Sending message", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         TextView testText = (TextView)findViewById(R.id.main_text);
         testText.setText(prefs.getString("pref_host", ""));
+
+        connection = new MqttConnection(this.getApplicationContext());
+
     }
 
     @Override
---
+++ app/src/main/java/pixento/nl/broadcasttomqtt/MqttConnection.java
@@ -0,0 +1,135 @@
+package pixento.nl.broadcasttomqtt;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+/**
+ * Created by Corniël Joosse on 25-Aug-17.
+ * Inspired by: https://github.com/eclipse/paho.mqtt.android/blob/master/paho.mqtt.android.example/src/main/java/paho/mqtt/java/example/PahoExampleActivity.java
+ */
+
+public class MqttConnection {
+
+    MqttAndroidClient mqttAndroidClient;
+
+    private String serverUri;
+    // private final String clientId = "BroadcastToMQTTAndroid";
+    private final String clientId = "BroadcastToMQTTAndroid";
+    private String publishTopic = "android/broadcast";
+    private static final String TAG = "MqttConnection";
+
+    public MqttConnection(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String server = prefs.getString("pref_host", "");
+        String port = prefs.getString("pref_port", "1883");
+        String username = prefs.getString("pref_username", "");
+        String password = prefs.getString("pref_password", "");
+        serverUri = "tcp://" + server + ":" + port;
+
+        // Create the MQTT client and set the callback class
+        mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+                    Log.i(TAG, "Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                    // subscribeToTopic();
+                } else {
+                    Log.i(TAG, "Connected to: " + serverURI);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.i(TAG, "The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.i(TAG, "Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        // mqttConnectOptions.setAutomaticReconnect(true);
+        // mqttConnectOptions.setCleanSession(false);
+        try {
+            mqttConnectOptions.setUserName(username);
+            mqttConnectOptions.setPassword(password.toCharArray());
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Log.i(TAG, "Connecting to " + serverUri);
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    // subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.i(TAG, "Failed to connect to: " + serverUri);
+                    exception.printStackTrace();
+                }
+            });
+
+
+        } catch (MqttException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    public void publish(String message) {
+        try {
+            MqttMessage mqttMessage = new MqttMessage();
+            mqttMessage.setPayload(message.getBytes());
+            mqttAndroidClient.publish(publishTopic, mqttMessage);
+
+            Log.i(TAG, "Message Published");
+            if (!mqttAndroidClient.isConnected()) {
+                Log.i(TAG, mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+            }
+        } catch (MqttException e) {
+            Log.i(TAG, "Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            mqttAndroidClient.disconnect();
+        } catch (MqttException e) {
+            Log.i(TAG, "Error disconnecting: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+}
